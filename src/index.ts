361802dfe027a6bede34565671c00fbdfc8c347d
QuantumRoshan <customuid10@gmail.com>
2024-03-13 14:35:45 +0545

    typescript

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+// let sales=2_34_6768;
+// let nameOf: string='don';
+// let c:any=8;
+// c='roshN';
+// console.log(c);
+ 
+
+let array: any[] =[1,2,3,4,5];
+array.forEach((e,i,a)=>{
+    a[i]=e.toString();
+})
+console.log(array);
